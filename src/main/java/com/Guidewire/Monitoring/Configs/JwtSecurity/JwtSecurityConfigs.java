fc2aa50e5f477b135dc07725b6f92e9b564ab359
ChawkiMersani <100194333+ChawkiMersani@users.noreply.github.com>
2024-05-25 15:28:04 +0100

    commit log test unitaire

---
+++ src/main/java/com/Guidewire/Monitoring/Configs/JwtSecurity/JwtSecurityConfigs.java
@@ -0,0 +1,60 @@
+package com.Guidewire.Monitoring.Configs.JwtSecurity;
+
+import com.Guidewire.Monitoring.Services.Implementations.UserDetailsCustomService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class JwtSecurityConfigs {
+
+    private final UserDetailsCustomService userDetailsCustomService;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public JwtSecurityConfigs(UserDetailsCustomService userDetailsCustomService, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userDetailsCustomService = userDetailsCustomService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req->req.requestMatchers("/user/signIn")
+                                .permitAll()
+                                //.requestMatchers("").hasAuthority("ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                ).userDetailsService(userDetailsCustomService)
+                .sessionManagement(session->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+}
